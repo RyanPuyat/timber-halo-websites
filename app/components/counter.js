4df32b67ef7a4201f9b5ce94bfb1f917bd599dd0
Ryan <ryanspuyat@gmail.com>
2025-09-26 22:29:39 +0900

    adding some functionality

--- app/components/counter.js
+++ app/components/counter.js
@@ -1,9 +1,9 @@
-'use client';
+// 'use client';
 
-import { useState } from 'react';
+// import { useState } from 'react';
 
-export default function Counter() {
-  const [count, setCount] = useState(0);
+// export default function Counter() {
+//   const [count, setCount] = useState(0);
 
-  return <button onClick={() => setCount((c) => c + 1)}>{count}</button>;
-}
+//   return <button onClick={() => setCount((c) => c + 1)}>{count}</button>;
+// }
