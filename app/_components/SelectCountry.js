24cecc900886289c822a5144bee7f8724c67d6cd
Ryan <ryanspuyat@gmail.com>
2025-10-08 15:12:55 +0900

    update guests functionality

--- app/_components/SelectCountry.js
+++ app/_components/SelectCountry.js
@@ -14,8 +14,9 @@ async function SelectCountry({ defaultCountry, name, id, className }) {
       // Here we use a trick to encode BOTH the country name and the flag into the value. Then we split them up again later in the server action
       defaultValue={`${defaultCountry}%${flag}`}
       className={className}
+      key={defaultCountry}
     >
-      <option value=''>Select country...</option>
+      <option value="">Select country...</option>
       {countries.map((c) => (
         <option key={c.name} value={`${c.name}%${c.flag}`}>
           {c.name}
--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,5 +1,7 @@
 'use server';
-import { signIn, signOut } from '@/app/_lib/auth';
+import { auth, signIn, signOut } from '@/app/_lib/auth';
+import { supabase } from './supabase';
+import { revalidatePath } from 'next/cache';
 
 export async function signInAction() {
   await signIn('google', { redirectTo: '/account' });
@@ -7,4 +9,31 @@ export async function signInAction() {
 
 export async function signOutAction() {
   await signOut({ redirectTo: '/' });
+}
+
+export async function updateGuest(formData) {
+  const session = await auth();
+  const regex = /^[a-zA-Z0-9]{6,12}$/;
+
+  if (!session) throw new Error('You must be logged in');
+
+  const nationalID = formData.get('nationalID');
+  const [nationality, countryFlag] = formData.get('nationality').split('%');
+
+  if (!regex.test(nationalID)) {
+    throw new Error('Please provide a valid national Id');
+  }
+
+  const updateData = { nationality, countryFlag, nationalID };
+
+  const { error } = await supabase
+    .from('guests')
+    .update(updateData)
+    .eq('id', session.user.guestId);
+
+  if (error) {
+    console.error(error);
+    throw new Error('Guest could not be updated');
+  }
+  revalidatePath('/account/profile');
 }
